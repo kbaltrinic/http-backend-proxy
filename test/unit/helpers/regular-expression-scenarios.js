889c756d176b0691df9bb2fb2d37ddd7ddcc0a27
Kenneth Baltrinic <kenneth@baltrinic.com>
2014-09-05 21:46:44 -0400

    Adds basic tests for serializing a regular expression created via constructor.

--- test/unit/helpers/regular-expression-scenarios.js
+++ test/unit/helpers/regular-expression-scenarios.js
@@ -7,5 +7,13 @@ module.exports = [
         regex : /find me/ig,
         output: '/find me/gi',
         desc  : 'litterals with modifiers'
+    }, {
+        regex : new RegExp('find me'),
+        output: '/find me/',
+        desc  : 'constructors'
+    }, {
+        regex : new RegExp('find me', 'ig'),
+        output: '/find me/gi',
+        desc  : 'constructors with modifiers'
     }
 ];
