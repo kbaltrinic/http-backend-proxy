1bfd4182e6e1a1b7613f03d157d4bf715f292c32
Kenneth Baltrinic <kenneth@baltrinic.com>
2014-09-05 23:01:18 -0400

    Adds tests to assert that regular expressions containing /forward/slashes are correctly serialized.

--- test/unit/helpers/regular-expression-scenarios.js
+++ test/unit/helpers/regular-expression-scenarios.js
@@ -15,5 +15,13 @@ module.exports = [
         regex : new RegExp('find me', 'ig'),
         output: "new RegExp('find me','gi')",
         desc  : 'constructors with modifiers'
+    }, {
+        regex : /\/a\/path/,
+        output: "new RegExp('\\/a\\/path')",
+        desc  : 'litterals with forward slashes'
+    }, {
+        regex : new RegExp('/a/path'),
+        output: "new RegExp('/a/path')",
+        desc  : 'constructors with forward slashes'
     }
 ];
